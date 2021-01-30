783ebc5bd796a5be6ccc4257d0b13ac8a1710b24
Glitch (lackadaisical-smoggy-judge) <none>
2021-01-30 20:31:45 +0000

    📮☃️ Checkpoint ./src/events/messages/music.js:16430712/649

---
+++ src/events/messages/music.js
@@ -0,0 +1,24 @@
+const BaseEvent = require('../../utils/structures/BaseEvent');
+const emojis = require("../../../emojis.json");
+let e = emojis;
+
+module.exports = class MessageEvent extends BaseEvent {
+  constructor() {
+    super('message');
+  }
+  
+  async run(client, message) {
+
+  if (message.author.bot) return; 
+
+  if (message.channel.id != "719920829751623700") return; // روم الميوزك  
+  if (message.author.id === client.user.id) return;
+
+      if (message.author.send) {
+    message.react('675632039050870785')
+    message.react('675632038547685386')
+   message.channel.send({files: ['https://cdn.probot.io/4wgr68igpP.png']})
+
+    }
+  } 
+}
