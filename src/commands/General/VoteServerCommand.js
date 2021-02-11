7d8c7d29599202d34c97ba9d856d6a542bb68fc0
Glitch (lackadaisical-smoggy-judge) <none>
2021-02-11 19:08:13 +0000

    🌓🐸 Checkpoint ./src/commands/General/VoteServerCommand.js:16430712/7036

--- src/commands/General/VoteServerCommand.js
+++ src/commands/General/VoteServerCommand.js
@@ -1,47 +1,22 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
 const { MessageEmbed } = require('discord.js');
-const { stripIndents } = require("common-tags");
-const { cyan } = require("../../../colours.json");
 const emojis = require("../../../emojis.json");
-let e = emojis;
+let e = emojis; 
 
-
-module.exports = class HelpModCommand extends BaseCommand {
+module.exports = class VoteCommand extends BaseCommand {
 	constructor() {
 		super('vt', 'Vote', ['vote-server'], 'vt', '­');
 	}
 
-  run(client, message, args, lang) {
+	run(client, message, args, lang) {
 
-    const help = new MessageEmbed()
-      .setAuthor(`${message.guild.me.displayName} | Version 1.0.0`)
-      .setColor(cyan)
-      .setTitle(`${e.folder} - Help Documents Overview :`)
-      .setThumbnail(client.user.displayAvatarURL())
-      .setFooter(`© ${message.guild.me.displayName}`, client.user.displayAvatarURL());
+		const embed = new MessageEmbed()
 
+      .setDescription(`> ** ${e.money} - صوت للبوت الآن عبر الرابط الموجود أدناه وأحصل على كريديت قد يصل إلى \`2700\` .** \n > ** ${e.error} -  ملاحظة : يمكنك التصويت كل 12 ساعة !**\n > **${e.channel}- http://i8.ae/z6kmK | ${message.author}**`)
+			.setColor('#5c0e48')
+			.setImage('https://cdn.probot.io/ELtAsaU0AP.gif')
 
-    if(!args[0]){
-      help.setDescription(`● Here you can find all \`${message.guild.me.displayName}\` Commands :\n ● Prefix : **${client.prefix}**`)
-      help.addField(`**${e.mod}・Vote Tracker Cmd - (7) :**`, '`ban`, `clear`, `font`, `kick`, `mute`, `say`, `unmute`') 
-     return message.channel.send(help);
-    } else{
-
-      let command = client.commands.get(args[0].toLowerCase());
-    
-
-      if(!command){
-        return message.channel.send(help.setTitle(`${e.error} - Invalid command !`).setDescription(`Use \`${client.prefix}help\` to see the list of commands .`));
-      }
-
-
-      help.setDescription(stripIndents`● Prefix: **${client.prefix}** 
-
-        **${e.general}・Command :** \`${command.name.slice(0, 1).toUpperCase() + command.name.slice(1)}\`
-        **${e.developer}・Aliases :**  \`${command.aliases ? command.aliases.join(", ") : "None" }\`
-        **${e.info}・Usage :** ${command.usage ? `\`${command.usage}\`` : "No usage"}`);
-
-      return message.channel.send(help);
-    }
-  }
+  		message.channel.send(embed).then(message => {message.delete({timeout: 10000})})
+//      help.addField(`**${e.mod}・Vote Tracker Cmd - (7) :**`, '`vt!vote`, `vt!votes`, `vt!leaderboard`, `vt!reminders`, `vt!reminders`, `vt!toggledm`, `vt!leaderboard`') 
+	}
 }
