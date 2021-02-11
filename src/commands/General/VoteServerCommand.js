63b788dc0421729acc15f6912ea0e65f282e6186
Glitch (lackadaisical-smoggy-judge) <none>
2021-02-11 20:33:23 +0000

    🏣🐾 Checkpoint ./src/commands/General/VoteServerCommand.js:16430712/89

--- src/commands/General/VoteServerCommand.js
+++ src/commands/General/VoteServerCommand.js
@@ -20,7 +20,7 @@ module.exports = class VoteCommand extends BaseCommand {
         { name: `> ${e.PSG} \` vt!reminders <on | off> \``, value: `> **${e.PSB} تمكين أو تعطيل التذكيرات التي ستذكرك كل 12 ساعة للتصويت **` },
         { name: `> ${e.PSG} \` vt!toggledm \``, value: `> **${e.PSB} تبديل ما إذا كنت تريد تلقي رسائل مباشرة لتأكيد التصويت أم لا **` },
         { name: `> ${e.PSG} \` vt!profile \``, value: `> **${e.PSB} لرؤية البروفايل الخاص بك **` },
-        { name: `> **${e.links}・Links :**`, value: "> **[Server](https://discord.gg/s-t) ● [Vote](http://discord.com/users/579173324425527296) ● [Donate](https://paypal.me/Hadikob)**" }
+        { name: `> **${e.links}・Links :**`, value: "> **[Server](https://discord.gg/s-t) ● [Vote](https://top.gg/servers/664614587714240512/vote) ● [Donate](https://paypal.me/Hadikob)**" }
       )
 
   		message.channel.send(embed)
