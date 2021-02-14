63900a6e71c63c7f13a62f41a71bd51b5aa65af2
Glitch (lackadaisical-smoggy-judge) <none>
2021-02-14 18:51:14 +0000

    🐠🙎 Checkpoint ./src/commands/Moderator/RolePubgCommand.js:16430712/2197

--- src/commands/Moderator/RolePubgCommand.js
+++ src/commands/Moderator/RolePubgCommand.js
@@ -10,23 +10,23 @@ module.exports = class EMuteCommand extends BaseCommand {
 
 	run(client, message, args) {
 
-    if (message.channel.type === 'dm') { return message.reply(`> **${e.error} - عذرا , لايمكنني تشغيل هذا الأمر في الخاص !**`); }
+    if (message.channel.type === 'dm') { return message.reply(`> **${e.error} - عذرا , لايمكنني تشغيل هذا الأمر في الخاص !**`).then(message => {message.delete({timeout: 5000})}) }
 
-    if ( !message.member.roles.cache.some(r=>["┃🏋🏻‍♀️︰Admins", "┃♖︰Moderators", "┃👩‍🔧︰Girls Mod", "┃👑︰Founders", "┃🔰︰Co Founder", "┃🛠︰Developpers", "┃⚡︰SYSTEM"].includes(r.name)) ) { return message.channel.send(`> **${e.error} - ${message.author} , ليس لديك الصلاحية لإستخدام هذا الأمر .**`) }
+    if ( !message.member.roles.cache.some(r=>["┃🏋🏻‍♀️︰Admins", "┃♖︰Moderators", "┃👩‍🔧︰Girls Mod", "┃👑︰Founders", "┃🔰︰Co Founder", "┃🛠︰Developpers", "┃⚡︰SYSTEM"].includes(r.name)) ) { return message.channel.send(`> **${e.error} - ${message.author} , ليس لديك الصلاحية لإستخدام هذا الأمر .**`).then(message => {message.delete({timeout: 10000})}) }
       else if (!args[0]) {
 
-      message.channel.send(`> **${e.error} - ${message.author} , يجب عليك ذكر الشخص الذي تريد إعطائه الرتبة .**`);
+      message.channel.send(`> **${e.error} - ${message.author} , يجب عليك ذكر الشخص الذي تريد إعطائه الرتبة .**`).then(message => {message.delete({timeout: 5000})})
 		} else {
 			try {
 				const muted = message.mentions.members.first(); 
 				const muter = message.author.tag; 
-				const channel = client.channels.cache.find(channel => channel.name === "rolepubg-logs"); 
+				const channel = client.channels.cache.find(channel => channel.name === "┃🕹️︰pubg"); 
 
 
         if (muted) {
 					
 					if (muted.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS']) && !message.member.hasPermission('ADMINISTRATOR')) {
-						message.channel.send(`> **${e.error} - ${message.author} , لا يمكنك إسكات هذا العضو !**`);
+						message.channel.send(`> **${e.error} - ${message.author} , لا يمكنك إعطاء الرتبة لهذا العضو !**`).then(message => {message.delete({timeout: 5000})})
 					} else {
 
             let mutedRole = message.guild.roles.cache.find(role => role.name === '┃⭐︰Pubg Events'); 
@@ -34,7 +34,7 @@ module.exports = class EMuteCommand extends BaseCommand {
 					
 						if (mutedRole) {
 							muted.roles.add(mutedRole); 
-							message.channel.send(`**${e.check} - ${muted} تم إعطاء الرتبة !**`);  
+							message.channel.send(`**${e.check} - ${muted} تم إعطاء الرتبة الخاصة بالمشاركة في الفعاليات إلى !**`).then(message => {message.delete({timeout: 5000})})
 
 						
 							const embed = new MessageEmbed()
@@ -50,8 +50,8 @@ module.exports = class EMuteCommand extends BaseCommand {
 							let AlertMuteEmbed = new MessageEmbed()
 
 							.setColor('#ff0000')
-								.setTitle(`${e.error} لقد تم قبولك ، وحصلت على رتبة المشاركة في الفعاليات : `)
-
+              	.setAuthor(`${message.guild.name}`, 'https://cdn.probot.io/INqoBWuLZa.png', 'https://discord.gg/s-t')
+                .setDescription(`** ${e.error} - لقد تم قبولك ، وحصلت على رتبة المشاركة في الفعاليات الخاصة بـ ببجي موبايل **`)
 						//		.setThumbnail('https://cdn.probot.io/INqoBWuLZa.png')
 								.setTimestamp()
 								.setFooter(`© ${message.guild.me.displayName}`, client.user.displayAvatarURL());
@@ -59,11 +59,11 @@ module.exports = class EMuteCommand extends BaseCommand {
 							muted.send(AlertMuteEmbed).catch(error => { console.error(`${error}`)})
 
 						} else {
-							message.channel.send(`> **${e.error} - ${message.author} , لا يمكنني العثور على رتبة الفعاليات .**`);
+							message.channel.send(`> **${e.error} - ${message.author} , لا يمكنني العثور على رتبة الفعاليات .**`).then(message => {message.delete({timeout: 5000})})
 						}
 					}
 				} else {
-					message.channel.send(`> **${e.error} - ${message.author} , العضو ليس موجود !**`);
+					message.channel.send(`> **${e.error} - ${message.author} , العضو ليس موجود !**`).then(message => {message.delete({timeout: 5000})})
 				}
 			} catch (e) {
 				console.error(e); 
