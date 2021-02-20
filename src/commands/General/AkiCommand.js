e2554a254fb5289909675cefb1fd33c2ab82de97
Glitch (lackadaisical-smoggy-judge) <none>
2021-02-20 16:33:26 +0000

    🎉⏳ Checkpoint ./src/commands/General/AkiCommand.js:16430712/36915

--- src/commands/General/AkiCommand.js
+++ src/commands/General/AkiCommand.js
@@ -1,7 +1,6 @@
-const emojis = ["👍", "👎", "❔", "🤔", "🙄", "❌"];
-const isPlaying = new Set();
-const { Aki } = require("aki-api");
-const { prefix, token } = require("./config");
+const number = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣'];
+const isPlayed = new Set();
+const AKI = require('aki-api')
 const BaseCommand = require('../../utils/structures/BaseCommand');
 const { MessageEmbed } = require('discord.js');
 const { stripIndents } = require("common-tags");
@@ -14,84 +13,109 @@ const Client = new Discord.Client();
 
 module.exports = class AvatarCommand extends BaseCommand {
 	constructor() {
-		super('avatar', 'General', ['picture'], 'picture', 'See a user avatar');
+		super('aki', 'General', ['picture'], 'picture', 'See a user avatar');
 	}
 
 	run(client, message, args, lang, prefix) {
+		const current = this.client.games.get(message.channel.id);
+		if (current) return message.reply(`Please wait until the current game of \`${current.name}\` is finished.`);
+		try {
+			const aki = new Aki(region, !message.channel.nsfw);
+			let ans = null;
+			let win = false;
+			let timesGuessed = 0;
+			let guessResetNum = 0;
+			let wentBack = false;
+			let forceGuess = false;
+			const guessBlacklist = [];
+			this.client.games.set(message.channel.id, { name: this.name });
+			while (timesGuessed < 3) {
+				if (guessResetNum > 0) guessResetNum--;
+				if (ans === null) {
+					 aki.start();
+				} else if (wentBack) {
+					wentBack = false;
+				} else {
+					try {
+						 aki.step(ans);
+					} catch {
+						 aki.step(ans);
+					}
+				}
+				if (!aki.answers || aki.currentStep >= 79) forceGuess = true;
+				const answers = aki.answers.map(answer => answer.toLowerCase());
+				answers.push('end');
+				if (aki.currentStep > 0) answers.push('back');
+				 message.say(stripIndents`
+					**${aki.currentStep + 1}.** ${aki.question} (${Math.round(Number.parseInt(aki.progress, 10))}%)
+					${aki.answers.join(' | ')}${aki.currentStep > 0 ? ` | Back` : ''} | End
+				`);
+				const filter = res => res.author.id === message.author.id && answers.includes(res.content.toLowerCase());
+				const messages =  message.channel.awaitMessages(filter, {
+					max: 1,
+					time: 30000
+				});
+				if (!messages.size) {
+					 message.say('Sorry, time is up!');
+					win = 'time';
+					break;
+				}
+				const choice = messages.first().content.toLowerCase();
+				if (choice === 'end') {
+					forceGuess = true;
+				} else if (choice === 'back') {
+					if (guessResetNum > 0) guessResetNum++;
+					wentBack = true;
+					 aki.back();
+					continue;
+				} else {
+					ans = answers.indexOf(choice);
+				}
+				if ((aki.progress >= 90 && !guessResetNum) || forceGuess) {
+					timesGuessed++;
+					guessResetNum += 10;
+					 aki.win();
+					const guess = aki.answers.filter(g => !guessBlacklist.includes(g.id))[0];
+					if (!guess) {
+						 message.say('I can\'t think of anyone.');
+						win = true;
+						break;
+					}
+					guessBlacklist.push(guess.id);
+					const embed = new MessageEmbed()
+						.setColor(0xF78B26)
+						.setTitle(`I'm ${Math.round(guess.proba * 100)}% sure it's...`)
+						.setDescription(stripIndents`
+							${guess.name}${guess.description ? `\n_${guess.description}_` : ''}
+							_**Type [y]es or [n]o to continue.**_
+						`)
+						.setThumbnail(guess.absolute_picture_path || null)
+						.setFooter(forceGuess ? 'Final Guess' : `Guess ${timesGuessed}`);
+					 message.embed(embed);
+					const verification =  verify(message.channel, message.author);
+					if (verification === 0) {
+						win = 'time';
+						break;
+					} else if (verification) {
+						win = false;
+						break;
+					} else if (timesGuessed >= 3 || forceGuess) {
+						win = true;
+						break;
+					} else {
+						 message.say('Hmm... Is that so? I can keep going!');
+					}
+				}
+			}
+			this.client.games.delete(message.channel.id);
+			if (win === 'time') return message.say('I guess your silence means I have won.');
+			if (win) return message.say('Bravo, you have defeated me.');
+			return message.say('Guessed right one more time! I love playing with you!');
+		} catch (err) {
+			this.client.games.delete(message.channel.id);
+			return message.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
 
-
-client
-  .on("ready", () => console.log("Ready!"))
-  .on("message", async message => {
-    if (message.author.bot || !message.guild) return;
-
-    if (!message.content.startsWith(prefix + "start")) return;
-
-    if (isPlaying.has(message.author.id)) {
-      return message.channel.send(":x: | The game already started..");
     }
-
-    isPlaying.add(message.author.id);
-
-    const aki = new Aki("ar"); // Full languages list at: https://github.com/jgoralcz/aki-api
-
-    await aki.start();
-
-    const msg = await message.channel.send(new MessageEmbed()
-      .setTitle(`${message.author.username}, Question ${aki.currentStep + 1}`)
-      .setColor("RANDOM")
-      .setDescription(`**${aki.question}**\n${aki.answers.map((an, i) => `${an} | ${emojis[i]}`).join("\n")}`));
-
-    for (const emoji of emojis) await msg.react(emoji);
-
-    const collector = msg.createReactionCollector((reaction, user) => emojis.includes(reaction.emoji.name) && user.id == message.author.id, {
-      time: 60000 * 6
-    });
-
-    collector
-      .on("end", () => isPlaying.delete(message.author.id))
-      .on("collect", async ({
-        emoji,
-        users
-      }) => {
-        users.remove(message.author).catch(() => null);
-
-        if (emoji.name == "❌") return collector.stop();
-
-        await aki.step(emojis.indexOf(emoji.name));
-
-        if (aki.progress >= 70 || aki.currentStep >= 78) {
-
-          await aki.win();
-
-          collector.stop();
-
-          message.channel.send(new MessageEmbed()
-            .setTitle("Is this your character?")
-            .setDescription(`**${aki.answers[0].name}**\n${aki.answers[0].description}\nRanking as **#${aki.answers[0].ranking}**\n\n[yes (**y**) / no (**n**)]`)
-            .setImage(aki.answers[0].absolute_picture_path)
-            .setColor("RANDOM"));
-
-          const filter = m => /(yes|no|y|n)/i.test(m.content) && m.author.id == message.author.id;
-
-          message.channel.awaitMessages(filter, {
-              max: 1,
-              time: 30000,
-              errors: ["time"]
-            })
-            .then(collected => {
-              const isWinner = /yes|y/i.test(collected.first().content);
-              message.channel.send(new MessageEmbed()
-                .setTitle(isWinner ? "Great! Guessed right one more time." : "Uh. you are win")
-                .setColor("RANDOM")
-                .setDescription("I love playing with you!"));
-            }).catch(() => null);
-        
-        } else {
-          msg.edit(new MessageEmbed()
-            .setTitle(`${message.author.username}, Question ${aki.currentStep + 1}`)
-            .setColor("RANDOM")
-            .setDescription(`**${aki.question}**\n${aki.answers.map((an, i) => `${an} | ${emojis[i]}`).join("\n")}`));
-        }
-      });
-  }).login(token);
+ 
+}
