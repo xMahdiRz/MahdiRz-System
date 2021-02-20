8104101531f44988230f17b67d1489c13ed3f8d6
Glitch (lackadaisical-smoggy-judge) <none>
2021-02-20 16:44:39 +0000

    🌼🍰 Checkpoint ./src/commands/General/AkiCommand.js:16430712/39475

--- src/commands/General/AkiCommand.js
+++ src/commands/General/AkiCommand.js
@@ -1,6 +1,3 @@
-const number = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣'];
-const isPlayed = new Set();
-const AKI = require('aki-api')
 const BaseCommand = require('../../utils/structures/BaseCommand');
 const { MessageEmbed } = require('discord.js');
 const { stripIndents } = require("common-tags");
@@ -10,112 +7,128 @@ let e = emojiss;
 const Discord = require("discord.js");
 const Client = new Discord.Client();
 
+const isPlaying = new Set();
+const { Aki } = require("aki-api");
+const akinatorAPI = require("aki-api");
+
 
 module.exports = class AvatarCommand extends BaseCommand {
 	constructor() {
 		super('aki', 'General', ['picture'], 'picture', 'See a user avatar');
 	}
 
-	run(client, message, args, lang, prefix) {
-		const current = this.client.games.get(message.channel.id);
-		if (current) return message.reply(`Please wait until the current game of \`${current.name}\` is finished.`);
-		try {
-			const aki = new Aki(region, !message.channel.nsfw);
-			let ans = null;
-			let win = false;
-			let timesGuessed = 0;
-			let guessResetNum = 0;
-			let wentBack = false;
-			let forceGuess = false;
-			const guessBlacklist = [];
-			this.client.games.set(message.channel.id, { name: this.name });
-			while (timesGuessed < 3) {
-				if (guessResetNum > 0) guessResetNum--;
-				if (ans === null) {
-					 aki.start();
-				} else if (wentBack) {
-					wentBack = false;
-				} else {
-					try {
-						 aki.step(ans);
-					} catch {
-						 aki.step(ans);
-					}
-				}
-				if (!aki.answers || aki.currentStep >= 79) forceGuess = true;
-				const answers = aki.answers.map(answer => answer.toLowerCase());
-				answers.push('end');
-				if (aki.currentStep > 0) answers.push('back');
-				 message.say(stripIndents`
-					**${aki.currentStep + 1}.** ${aki.question} (${Math.round(Number.parseInt(aki.progress, 10))}%)
-					${aki.answers.join(' | ')}${aki.currentStep > 0 ? ` | Back` : ''} | End
-				`);
-				const filter = res => res.author.id === message.author.id && answers.includes(res.content.toLowerCase());
-				const messages =  message.channel.awaitMessages(filter, {
-					max: 1,
-					time: 30000
-				});
-				if (!messages.size) {
-					 message.say('Sorry, time is up!');
-					win = 'time';
-					break;
-				}
-				const choice = messages.first().content.toLowerCase();
-				if (choice === 'end') {
-					forceGuess = true;
-				} else if (choice === 'back') {
-					if (guessResetNum > 0) guessResetNum++;
-					wentBack = true;
-					 aki.back();
-					continue;
-				} else {
-					ans = answers.indexOf(choice);
-				}
-				if ((aki.progress >= 90 && !guessResetNum) || forceGuess) {
-					timesGuessed++;
-					guessResetNum += 10;
-					 aki.win();
-					const guess = aki.answers.filter(g => !guessBlacklist.includes(g.id))[0];
-					if (!guess) {
-						 message.say('I can\'t think of anyone.');
-						win = true;
-						break;
-					}
-					guessBlacklist.push(guess.id);
-					const embed = new MessageEmbed()
-						.setColor(0xF78B26)
-						.setTitle(`I'm ${Math.round(guess.proba * 100)}% sure it's...`)
-						.setDescription(stripIndents`
-							${guess.name}${guess.description ? `\n_${guess.description}_` : ''}
-							_**Type [y]es or [n]o to continue.**_
-						`)
-						.setThumbnail(guess.absolute_picture_path || null)
-						.setFooter(forceGuess ? 'Final Guess' : `Guess ${timesGuessed}`);
-					 message.embed(embed);
-					const verification =  verify(message.channel, message.author);
-					if (verification === 0) {
-						win = 'time';
-						break;
-					} else if (verification) {
-						win = false;
-						break;
-					} else if (timesGuessed >= 3 || forceGuess) {
-						win = true;
-						break;
-					} else {
-						 message.say('Hmm... Is that so? I can keep going!');
-					}
-				}
-			}
-			this.client.games.delete(message.channel.id);
-			if (win === 'time') return message.say('I guess your silence means I have won.');
-			if (win) return message.say('Bravo, you have defeated me.');
-			return message.say('Guessed right one more time! I love playing with you!');
-		} catch (err) {
-			this.client.games.delete(message.channel.id);
-			return message.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
-		}
+	run(client, message, args, lang, prefix, questionTitle, question, answers, channel, user, progress) {
+
+
+    const messageEmbed = new MessageEmbed()
+        .setTitle(questionTitle || "Question")
+        .setDescription(question)
+        .addField("Answers:", (answers.join(", ")))
+        .addField("Progress", `${Math.floor(progress || 0)}%`)
+        .setFooter(user.tag, user.displayAvatarURL)
+        .setColor(0x5B94FF);
+
+    const askMessage =  channel.send(messageEmbed);
+    const retrievedAnswer =  channel.awaitMessages(response => response.member.id === user.id, {
+        max: 1,
+        time: 30000,
+        errors: ["time"],
+    }).catch(e => { console.error(e); });
+
+    return [askMessage, retrievedAnswer && retrievedAnswer.first() && retrievedAnswer.first().content || null];
+}
+
+
+/*exports.run = async (client, message, args, { guild, user, error }) => {
+    const { prefix } = guild;
+
+    if (!args.length) return message.channel.send(
+        new RichEmbed().setTitle("Akinator").setColor(Colors.FAILED).setDescription("You must include a valid language (i.e: en = english).").setFooter(message.author.tag, message.author.displayAvatarURL),
+    );
+
+    const data = args.join(" ").split(/(\||;)/).map(i => i.trim());
+
+    // -- Game data
+
+    const providedRegion = data[0];
+    const akinatorGame = await akinatorAPI.start(providedRegion);
+    let currentQuestion = akinatorGame;
+    let currentProgress = 0;
+    let gameAborted = false;
+    let gameSolved = false;
+    let guessed;
+
+    // -- The game loop
+    async function startGameLoop(force) {
+        while ((currentProgress < QUESTION_TRESHOLD && force !== true || !currentQuestion && force !== true) || force === true) {
+            if (force === true) force = false;
+
+            // -- Using askQuestion to ask the question and getting the response
+            // -> If you want to do something with the question message, you can use "askMessage". I.e askMessage.delete();
+            const [askMessage, answer] = await askQuestion(null, currentQuestion.question || currentQuestion.nextQuestion, currentQuestion.answers, message.channel, message.author, currentProgress);
+
+            // -- If no answer was given in time, stopping the game
+            if (!answer) {
+                const stopEmbed = new RichEmbed()
+                    .setTitle("Game stopped")
+                    .setDescription("As no answer was given, the game was stopped!")
+                    .setColor(Colors.HAZARD)
+                    .setFooter(message.author.tag, message.author.displayAvatarURL);
+                await message.channel.send(stopEmbed);
+                gameAborted = true;
+                return;
+            } else if (answer.toLowerCase() === "cancel") {
+                const stopEmbed = new RichEmbed()
+                    .setTitle("Game stopped")
+                    .setDescription("The player has chosen to cancel the game.")
+                    .setColor(Colors.HAZARD)
+                    .setFooter(message.author.tag, message.author.displayAvatarURL);
+                await message.channel.send(stopEmbed);
+                gameAborted = true;
+                return;
+            } else {
+                const answersLowercase = currentQuestion.answers.map(x => x.toLowerCase());
+                let foundAnswer = answersLowercase.indexOf(answer.toLowerCase());
+                foundAnswer = foundAnswer > -1 ? foundAnswer : 0;
+
+                currentQuestion = await akinatorAPI.step(providedRegion, akinatorGame.session, akinatorGame.signature, foundAnswer, currentQuestion.nextStep || 0);
+                currentProgress = Number(currentQuestion.progress || "0");
+            }
+        }
+    }
+
+    // -- Controlling the game loop, lasts until the game is finished or aborted
+
+    while (gameSolved === false) {
+        await startGameLoop();
+        guessed = await akinatorAPI.win(providedRegion, akinatorGame.session, akinatorGame.signature, currentQuestion.nextStep).catch(e => {console.log(e);});
+
+        const firstGuess = guessed && guessed.answers && guessed.answers[0] || null;
+
+        if (firstGuess) {
+            gameSolved = true;
+            guessed = firstGuess;
+            break;
+        } else if (!gameAborted === true) {
+            await startGameLoop(true);
+        }
+    }
+
+    // -- If the game wasn"t aborted, but rather finished then proceed
+    if (gameAborted !== true) {
+
+        const guessEmbed = new RichEmbed()
+            .setTitle("Character guessed!")
+            .addField("Character name", guessed.name, true)
+            .addField("Probability", `${Math.floor(100 * (Number(guessed.proba || "100")))}%`, true)
+            .addField("Description", guessed.description)
+            .setImage(guessed.absolute_picture_path)
+            .setFooter(message.author.tag, message.author.displayAvatarURL)
+            .setColor(Colors.DEFAULT);
+
+        message.channel.send(guessEmbed);
 
     }
- 
-}
+
+*/
+  }
