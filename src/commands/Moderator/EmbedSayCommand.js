bdc840a9d33e9ca9d0b2d7e2153188284e869cfa
Glitch (lackadaisical-smoggy-judge) <none>
2021-02-23 21:50:48 +0000

    👛🎣 Checkpoint ./src/commands/Moderator/EmbedSayCommand.js:16430712/262

--- src/commands/Moderator/EmbedSayCommand.js
+++ src/commands/Moderator/EmbedSayCommand.js
@@ -25,7 +25,7 @@ module.exports = class SayCommand extends BaseCommand {
 
     	const embed = new MessageEmbed()
 			.setDescription(argsResult)
-			.setColor('#92c7cf');
+			.setColor('RANDOM');
   
       channelName.send(embed); 
 
@@ -35,7 +35,7 @@ module.exports = class SayCommand extends BaseCommand {
 
     	const embed = new MessageEmbed()
 			.setDescription(argsResult)
-			.setColor('#92c7cf');
+			.setColor('RANDOM');
 
       message.channel.send(embed); 
     }
