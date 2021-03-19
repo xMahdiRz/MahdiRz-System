07b51f333c652c4e1faa089982a5f11a8209d847
Glitch (lackadaisical-smoggy-judge) <none>
2021-03-19 19:46:25 +0000

    🛰🛎 Checkpoint ./src/events/message/message.js:16430712/1174

--- src/events/message/message.js
+++ src/events/message/message.js
@@ -21,11 +21,27 @@ if(message.content.startsWith('خط')){
 		message.delete();
     message.channel.send({files: ['https://cdn.probot.io/4wgr68igpP.png']});  }
 
-if(message.content.startsWith('خط')){
+if(message.content.startsWith('ar')){
 		if (message.author.id !== '579173324425527296' && message.author.id !== '557711715610001428' ) return;
-		message.delete();
-    message.channel.send({files: ['https://cdn.probot.io/4wgr68igpP.png']}); 
 
+		message.delete();
+		const embed = new MessageEmbed()
+
+	.setColor('#0099ff')
+	.setAuthor('xMuSaBx l مصعب', 'https://cdn.probot.io/NOhdjD7O9i.png')
+	.setDescription(`> <a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764>
+> **<a:MG_Youtube:821790535852490782> | Youtube : [xMuSaBx l مصعب](https://bit.ly/xMuSaBx-YT)**
+> **<a:MG_Facebook:821790533818908732> | Facebook : [XMusabOC](https://bit.ly/xMuSaBx-Fb)**
+> **<a:MG_Instagram:821790546107957290> | Instagram : [@xmus3bx](https://bit.ly/xMuSaBx-Inst)**
+> **<a:MG_Discord:764954741209432124> | Discord : <@!332188711473119232> **
+> <a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764>
+`)
+	//.setThumbnail('https://cdn.probot.io/NOhdjD7O9i.png')
+	.setImage('https://cdn.probot.io/c1E7IO0RX9.jpg')
+
+
+
+  message.channel.send(embed)
 
 
  }
