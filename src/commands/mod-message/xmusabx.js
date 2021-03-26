cb0fbed50f9972d0642bda5af82005c96801f05d
Glitch (lackadaisical-smoggy-judge) <none>
2021-03-26 18:55:18 +0000

    🌇⛲️ Checkpoint ./src/commands/mod-message/xmusabx.js:16430712/2021

--- src/commands/mod-message/xmusabx.js
+++ src/commands/mod-message/xmusabx.js
@@ -30,7 +30,6 @@ module.exports = class ArtsRulesCommand extends BaseCommand {
 > **<a:MG_Discord:825030310507839488> | Discord : <@!332188711473119232> **
 > <a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764>
 `)
-	//.setThumbnail('https://cdn.probot.io/NOhdjD7O9i.png')
 	.setImage('https://cdn.probot.io/c1E7IO0RX9.jpg')
 
   message.channel.send(xmusabx)
@@ -49,7 +48,6 @@ module.exports = class ArtsRulesCommand extends BaseCommand {
 > **<a:MG_Discord:825030310507839488> | Discord : <@!291953829572902912> **
 > <a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764>
 `)
-	//.setThumbnail('https://cdn.probot.io/NOhdjD7O9i.png')
 	.setImage('https://yt3.ggpht.com/UKaq-Y8knORoMzl7jVvG58JoxTfnTVxKJlgLOBtduCFwsZupywYSZbWoxPzDDx8QwpCCzyuzhA=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj')
 
   message.channel.send(xsame7x)
@@ -59,16 +57,30 @@ module.exports = class ArtsRulesCommand extends BaseCommand {
 	.setColor('#0099ff')
 	.setAuthor('Mnaiir l مناير', 'https://yt3.ggpht.com/ytc/AAUvwng4uqWFtqsnLUde02vymOUlr8IqP8Z5w5bwv5yrHw=s88-c-k-c0x00ffffff-no-rj')
 	.setDescription(`> <a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764>
-> **<a:MG_Youtube:821790535852490782> | Youtube : [منـآير•MnAiiR](https://bit.ly/xSaMe7x-YT)**
-> **<a:MG_Twitch:825069776216784976> | Twitch : [imnaiiri](https://www.twitch.tv/imnaiiri)**
+> **<a:MG_Youtube:821790535852490782> | Youtube : [منـآير•MnAiiR](https://bit.ly/MNAIIR-YT)**
+> **<a:MG_Twitch:825069776216784976> | Twitch : [imnaiiri](https://bit.ly/MNAIIR-Inst)**
 > **<a:MG_Instagram:821790546107957290> | Instagram : [@7ll.11](https://bit.ly/xSaMe7x-Inst)**
 > **<a:MG_Discord:825030310507839488> | Discord : <@!379643020137725972> **
 > <a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764>
 `)
-	//.setThumbnail('https://cdn.probot.io/NOhdjD7O9i.png')
-	.setImage('https://yt3.ggpht.com/nbOWtGSGV1B_XQZHc6DizCQ5QT9lmP_isDlXm9jcHv_GGKIvyHPTOQdo4eGqo2auM7jnufwD=w1280-fcrop64=1,00000000ffffffff-k-c0xffffffff-no-nd-rj')
+	.setImage('https://yt3.ggpht.com/nbOWtGSGV1B_XQZHc6DizCQ5QT9lmP_isDlXm9jcHv_GGKIvyHPTOQdo4eGqo2auM7jnufwD=w1280-fcrop64=1,32b75a57cd48a5a8-k-c0xffffffff-no-nd-rj')
+  message.channel.send(mnaiir)
 
-  message.channel.send(xsame7x)
+const bulahab = new MessageEmbed()
 
+	.setColor('RED')
+	.setAuthor('Bulahab l بولهب', 'https://cdn.probot.io/1YtFk59Bdk.png')
+	.setDescription(`> <a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764>
+> **<a:MG_Youtube:821790535852490782> | Youtube : [BuLahaB I بولهب](https://bit.ly/xSaMe7x-YT)**
+> **<a:MG_Twitter:825055749105254441> | Twitter : [@bu_laha](https://bit.ly/xSaMe7x-Twit)**
+> **<a:MG_Snapchat:825048684264816720> | Snapchat : [@alkanzoo](https://bit.ly/xSaMe7x-Fb)**
+> **<a:MG_Instagram:821790546107957290> | Instagram : [@r.ak9](https://bit.ly/xSaMe7x-Inst)**
+> **<a:MG_Pubg:825036799888982026> | Pubg ID : \`5192027638\` **
+> **<a:MG_Discord:825030310507839488> | Discord : <@!590671974863470592> **
+> <a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764>
+`)
+	.setImage('https://yt3.ggpht.com/UKaq-Y8knORoMzl7jVvG58JoxTfnTVxKJlgLOBtduCFwsZupywYSZbWoxPzDDx8QwpCCzyuzhA=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj')
+
+  message.channel.send(bulahab)
  } 
  }
