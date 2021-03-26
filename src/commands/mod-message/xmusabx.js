8893cfa8a72128334ed634333db1f1da4913b9bc
Glitch (lackadaisical-smoggy-judge) <none>
2021-03-26 18:43:49 +0000

    🕍🎍 Checkpoint ./src/commands/mod-message/xmusabx.js:16430712/2383

--- src/commands/mod-message/xmusabx.js
+++ src/commands/mod-message/xmusabx.js
@@ -38,19 +38,35 @@ module.exports = class ArtsRulesCommand extends BaseCommand {
 
 		const xsame7x = new MessageEmbed()
 
-	.setColor('#0099ff')
+	.setColor('RED')
 	.setAuthor('xSaMe7x l سامح', 'https://cdn.probot.io/1YtFk59Bdk.png')
 	.setDescription(`> <a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764>
-> **<a:MG_Youtube:821790535852490782> | Youtube : [xSaMe7x l سامح](https://bit.ly/xMuSaBx-YT)**
-> **<a:MG_Twitch:825069776216784976> | Twitch : [xmus3bx](https://bit.ly/xMuSaBx-Twi)**
-> **<a:MG_Facebook:821790533818908732> | Facebook : [XSaMe7x I سامح](https://bit.ly/xMuSaBx-Fb)**
-> **<a:MG_Instagram:821790546107957290> | Instagram : [@xSaMe7x](https://bit.ly/xMuSaBx-Inst)**
+> **<a:MG_Youtube:821790535852490782> | Youtube : [xSaMe7x l سامح](https://bit.ly/xSaMe7x-YT)**
+> **<a:MG_Twitch:825069776216784976> | Twitch : [xSaMe7x](https://bit.ly/xSaMe7x-Twit)**
+> **<a:MG_Facebook:821790533818908732> | Facebook : [XSaMe7x I سامح](https://bit.ly/xSaMe7x-Fb)**
+> **<a:MG_Instagram:821790546107957290> | Instagram : [@xSaMe7x](https://bit.ly/xSaMe7x-Inst)**
+> **<a:MG_Pubg:825036799888982026> | Pubg ID : \`5177071266\` **
 > **<a:MG_Discord:825030310507839488> | Discord : <@!291953829572902912> **
-> **<a:MG_Pubg:825036799888982026> | Discord : <@!291953829572902912> **
 > <a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764>
 `)
 	//.setThumbnail('https://cdn.probot.io/NOhdjD7O9i.png')
 	.setImage('https://yt3.ggpht.com/UKaq-Y8knORoMzl7jVvG58JoxTfnTVxKJlgLOBtduCFwsZupywYSZbWoxPzDDx8QwpCCzyuzhA=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj')
+
+  message.channel.send(xsame7x)
+
+		const mnaiir = new MessageEmbed()
+
+	.setColor('#0099ff')
+	.setAuthor('Mnaiir l مناير', 'https://yt3.ggpht.com/ytc/AAUvwng4uqWFtqsnLUde02vymOUlr8IqP8Z5w5bwv5yrHw=s88-c-k-c0x00ffffff-no-rj')
+	.setDescription(`> <a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764>
+> **<a:MG_Youtube:821790535852490782> | Youtube : [منـآير•MnAiiR](https://bit.ly/xSaMe7x-YT)**
+> **<a:MG_Twitch:825069776216784976> | Twitch : [imnaiiri](https://www.twitch.tv/imnaiiri)**
+> **<a:MG_Instagram:821790546107957290> | Instagram : [@7ll.11](https://bit.ly/xSaMe7x-Inst)**
+> **<a:MG_Discord:825030310507839488> | Discord : <@!379643020137725972> **
+> <a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764><a:370:748978646005448764>
+`)
+	//.setThumbnail('https://cdn.probot.io/NOhdjD7O9i.png')
+	.setImage('https://yt3.ggpht.com/nbOWtGSGV1B_XQZHc6DizCQ5QT9lmP_isDlXm9jcHv_GGKIvyHPTOQdo4eGqo2auM7jnufwD=w1280-fcrop64=1,00000000ffffffff-k-c0xffffffff-no-nd-rj')
 
   message.channel.send(xsame7x)
 
